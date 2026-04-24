07835f0d2b3c834b5302f7bad53c99f90d125b32
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-24 09:25:13 -0400

    First Setup Created class with getters. Added User Input scanner and selection choices.

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the online store! ");
+
+        System.out.println("Select from the following options: ");
+        System.out.println("\t1) Display Products");
+        System.out.println("\t2Display Cart");
+        System.out.println("\t0 Exit") ;
+        System.out.println("Enter your selection: ");
+        int UserOption = scanner.nextInt();
+
+
+
 
     }
 }
