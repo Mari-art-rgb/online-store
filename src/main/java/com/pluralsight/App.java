d008f5633b8bd732113c3e42457ba6684065b382
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-24 10:17:40 -0400

    First Setup Created class with getters. Added User Input scanner and selection choices. Put display options for the user. Put the display choices in a while loop and fixed the Scanner. Created fileReader to read products file.

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +12,7 @@ public class App {
         System.out.println("Welcome to the online store! ");
 
         homeScreen(scanner);
+
 
     }
     //Passing in the scanner so it's not red
@@ -24,7 +28,8 @@ public class App {
 
             switch (userOption) {
                 case 1:
-                    //  displayProducts(scanner);
+                   displayProducts(scanner);
+                    formatSpace();
                     break;
 
                 case 2:
@@ -34,16 +39,31 @@ public class App {
                     System.out.println("Thanks for visting my online store. ");
                     scanner.close();
                     System.exit(0);
+                    formatSpace();
 
                     break;
 
                 default:
                     System.out.println("Incorrect option entered");
                     scanner.nextLine();
+                    formatSpace();
             }
 
 
         }
+    }
+    public static void displayProducts(Scanner scanner){
+        try{
+            BufferedReader buffReader = new BufferedReader(new FileReader("src/main/resources/products.csv"));
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void formatSpace() {
+        System.out.println("\n\n");
     }
 
 
