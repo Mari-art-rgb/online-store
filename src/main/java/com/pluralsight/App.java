9def8e38fd453eabd7c5cfa813ac04c091b57123
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-24 09:54:21 -0400

    First Setup Created class with getters. Added User Input scanner and selection choices. Put display options for the user. Put the display choices in a while loop and fixed the Scanner.

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -8,32 +8,43 @@ public class App {
 
         System.out.println("Welcome to the online store! ");
 
-        System.out.println("Select from the following options: ");
-        System.out.println("\t1) Display Products");
-        System.out.println("\t2Display Cart");
-        System.out.println("\t0 Exit") ;
-        System.out.println("Enter your selection: ");
-        int UserOption = scanner.nextInt();
-
-        switch (userOption) {
-            case 1:
-                displayProducts();
-                break;
-
-            case 2:
-                displayCart();
-            case 0:
-                //exit?
-                System.out.println("Thanks for visting my online store. ");
-                System.exit(0);
-                break;
-
-            default:
-                System.out.println("Incorrect option entered");
-        }
-
-
-
+        homeScreen(scanner);
 
     }
+    //Passing in the scanner so it's not red
+    public static void homeScreen(Scanner scanner) {
+        while (true) {
+
+            System.out.println("Select from the following options: ");
+            System.out.println("\t1) Display Products");
+            System.out.println("\t2)Display Cart");
+            System.out.println("\t0)Exit");
+            System.out.println("Enter your selection: ");
+            int userOption = scanner.nextInt();
+
+            switch (userOption) {
+                case 1:
+                    //  displayProducts(scanner);
+                    break;
+
+                case 2:
+                    //   displayCart(scanner);
+                case 0:
+                    //exit?
+                    System.out.println("Thanks for visting my online store. ");
+                    scanner.close();
+                    System.exit(0);
+
+                    break;
+
+                default:
+                    System.out.println("Incorrect option entered");
+                    scanner.nextLine();
+            }
+
+
+        }
+    }
+
+
 }
